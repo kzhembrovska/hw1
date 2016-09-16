e1778ebabdb15f617daf527f1db0db91a0348986
kzhembrovska <kzhembrovska@cca.edu>
2016-09-15 17:59:31 -0700

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,47 @@
+function pleasework(){
+  if (getColor()=="red"){
+   right(); right();
+    up();up();up();up();up();up();up();up();up();
+}else{ 
+  down();
+}}
+
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
+pleasework();
